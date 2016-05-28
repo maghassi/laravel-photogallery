a44b7c1d64594b2f9d1dc289fb34a2f81fa63ac6
Murad <murad.aqasiyev@gmail.com>
2016-05-28 10:39:51 +0000

    DB setup and gallery store function

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+use DB;
+
 class GalleryController extends Controller
 {
     public function index()
@@ -21,7 +25,35 @@ class GalleryController extends Controller
     
     public function store(Request $request)
     {
-        //
+        //get request data
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $cover_image = $request->file('cover_image');
+        $owner_id = 1;
+        
+        //check if uploaded
+        if($cover_image)
+        {
+            $cover_image_filename = $cover_image->getClientOriginalName();
+            $cover_image->move(public_path('/images'), $cover_image_filename);
+        }
+        else
+        {
+            $cover_image_filename = 'noimage.jpg';
+        }
+        
+         //Insert into DB
+        DB::table('galleries')->insert(
+            [
+              'name' => $name,
+              'description' => $description,
+              'cover_image' => $cover_image_filename,
+              'owner_id' => $owner_id
+          ]
+        );
+        
+        //Redirect
+        return \Redirect::route('gallery.index')->with('messsage', 'Gallery Created');
     }
     
     public function show($id)
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -32,6 +32,14 @@
               <span class="title-bar-title">Mike Mikerson</span>
             </div>
           </div>
+          
+          @if(Session::has('message'))
+          
+             <div class = "alert alert-info">
+                 {{Session::get('message')}}
+             </div>
+          
+          @endif
 
           
           @yield('content')
